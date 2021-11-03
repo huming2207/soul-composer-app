6ed07d8d3bc025499c3f42460d0cef97698fd0ef
Jackson Ming Hu <huming2207@gmail.com>
2021-11-03 23:01:26 +1100

    frontend: test out detect_device API

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { invoke } from '@tauri-apps/api/tauri';
 
 const app = document.getElementById('app');
 ReactDOM.render(<App />, app);
+
+setInterval(() => {
+  invoke('detect_device')
+    .then((ret) => {
+      console.log(ret);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}, 3000);

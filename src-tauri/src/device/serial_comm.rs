0b3d253faa0a82e07ba75db423baffd783392800
Jackson Ming Hu <huming2207@gmail.com>
2021-11-04 22:47:07 +1100

    backend: serial_comm: no more async for now

--- src-tauri/src/device/serial_comm.rs
+++ src-tauri/src/device/serial_comm.rs
@@ -1,59 +1,42 @@
-use futures::{stream::StreamExt, SinkExt};
-use slip_codec::tokio::SlipCodec;
-use tokio::task::JoinHandle;
-use tokio_serial::{SerialPortBuilderExt, SerialStream};
-use tokio_util::codec::Framed;
+use serialport::SerialPort;
 
 use crate::device::error::DeviceError;
 
 pub struct SerialComm {
     pub serial_port: String,
-    serial: SerialStream,
-    read_task: Option<JoinHandle<()>>,
+    serial: Box<dyn SerialPort>,
 }
 
 impl SerialComm {
     pub fn new(port: String) -> Result<SerialComm, DeviceError> {
-        let mut serial = tokio_serial::new(&port, 115200)
-            .open_native_async()
-            .map_err(|err| DeviceError::DeviceOpenError(err.to_string()))?;
-
-        #[cfg(unix)]
-        serial
-            .set_exclusive(true)
-            .map_err(|err| DeviceError::DeviceOpenError(err.to_string()))?;
+        let mut serial = serialport::new(&port, 115200).open().map_err(|err| DeviceError::OpenError(err.to_string()))?;
 
         Ok(SerialComm {
             serial,
             serial_port: port,
-            read_task: None,
         })
     }
 
-    pub async fn read(&mut self) -> Result<Vec<u8>, DeviceError> {
-        let serial = &mut self.serial;
-        let mut reader = Framed::new(serial, SlipCodec::new());
-        let mut buf: Vec<u8> = Vec::new();
-        while let Some(result) = reader.next().await {
-            let mut data = match result {
-                Ok(data) => data,
-                Err(_) => return Err(DeviceError::ReadError("SLIP decode fail".to_string())),
-            };
-
-            buf.extend_from_slice(data.as_mut());
+    pub fn read(&mut self) -> Result<Vec<u8>, DeviceError> {
+        let len = self.serial.bytes_to_read().map_err(|err| DeviceError::ReadError(err.to_string()))?;
+        if len < 1 {
+            return Err(DeviceError::NothingToRead);
         }
 
-        Ok(buf)
+        let buf: Vec<u8> = vec![0; len as usize];
+        let read_len = self.serial.read(&mut buf).map_err(|err| DeviceError::ReadError(err.to_string()))?;
+        
+        if read_len < 1 {
+            return Err(DeviceError::NothingToRead);
+        } else {
+            return Ok(buf);
+        }
     }
 
-    pub async fn write(&mut self, data: &[u8]) -> Result<(), DeviceError> {
-        let serial = &mut self.serial;
-        let mut writer = Framed::new(serial, SlipCodec::new());
-
-        let bytes = bytes::Bytes::copy_from_slice(data);
-        match writer.send(bytes).await {
-            Ok(_) => Ok(()),
-            Err(_) => return Err(DeviceError::ReadError("SLIP encode fail".to_string())),
+    pub fn write(&mut self, data: &[u8]) -> Result<usize, DeviceError> {
+        match self.serial.write(data) {
+            Ok(ret) => Ok(ret),
+            Err(err) => Err(DeviceError::WriteError(err.to_string()))
         }
     }
 }

1918cd3d97452aa600f3f804b9301b6db4249047
Jackson Ming Hu <huming2207@gmail.com>
2021-11-06 23:14:20 +1100

    backend: serial_comm: add SLIP

--- src-tauri/src/device/serial_comm.rs
+++ src-tauri/src/device/serial_comm.rs
@@ -1,5 +1,6 @@
 use std::sync::{Arc, Mutex};
 
+use serial_line_ip::{Decoder, Encoder};
 use serialport::{SerialPort, TTYPort};
 use crate::device::error::DeviceError;
 
@@ -33,13 +34,26 @@ impl SerialComm {
         if read_len < 1 {
             return Err(DeviceError::NothingToRead);
         } else {
-            return Ok(buf);
+            let mut decoder = Decoder::new();
+            let mut output_slice: Vec<u8> = vec![0; read_len];
+            let (_bytes_processed, output, is_eop) = decoder.decode(&buf, &mut output_slice).map_err(|err| DeviceError::ReadError(err.to_string()))?;
+
+            if is_eop {
+                return Ok(output.to_vec());
+            } else {
+                return Err(DeviceError::ReadError("Packet not ended".to_string()))
+            }
         }
     }
 
     pub fn write(&self, data: &[u8]) -> Result<usize, DeviceError> {
         let mut serial = self.serial.try_clone().map_err(|err| DeviceError::ReadError(err.to_string()))?;
-        match serial.write(data) {
+        let mut output: Vec<u8> = vec![0; data.len() * 2 + 2]; // Worst case senario is 2x of the original buffer (assuming someone send all ESC and/or END) + 2 of 0xC0
+        let mut encoder = Encoder::new();
+        let encoded = encoder.encode(data, &mut output).map_err(|err| DeviceError::ReadError(err.to_string()))?;
+
+
+        match serial.write(&output[..encoded.written]) {
             Ok(ret) => Ok(ret),
             Err(err) => Err(DeviceError::WriteError(err.to_string()))
         }

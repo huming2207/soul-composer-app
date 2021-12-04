c2b4b19002f109772e946cb42a573233484407b6
Jackson Ming Hu <huming2207@gmail.com>
2021-12-04 12:11:45 +1100

    backend: add CRC32 hasher

--- src-tauri/src/device/packet/mod.rs
+++ src-tauri/src/device/packet/mod.rs
@@ -4,5 +4,6 @@ pub mod file_chunk;
 pub mod misc;
 pub mod pkt_header;
 
-use crc::{Crc, CRC_16_XMODEM};
+use crc::{Crc, CRC_16_XMODEM, CRC_32_CKSUM};
 pub const CDC_CRC: Crc<u16> = Crc::<u16>::new(&CRC_16_XMODEM);
+pub const BLOB_CRC: Crc<u32> = Crc::<u32>::new(&CRC_32_CKSUM);

b2c8f92f56008f9182491fa09dfa5c63bb7fa105
Jackson Ming Hu <huming2207@gmail.com>
2021-12-14 22:39:56 +1100

    backend: use correct CRC32 algorithm

--- src-tauri/src/device/packet/mod.rs
+++ src-tauri/src/device/packet/mod.rs
@@ -6,9 +6,9 @@ pub mod pkt_header;
 
 use std::convert::TryInto;
 
-use crc::{Crc, CRC_16_XMODEM, CRC_32_CKSUM};
+use crc::{Crc, CRC_16_XMODEM, CRC_32_ISO_HDLC};
 pub const CDC_CRC: Crc<u16> = Crc::<u16>::new(&CRC_16_XMODEM);
-pub const BLOB_CRC: Crc<u32> = Crc::<u32>::new(&CRC_32_CKSUM);
+pub const BLOB_CRC: Crc<u32> = Crc::<u32>::new(&CRC_32_ISO_HDLC);
 
 #[inline]
 pub(crate) fn slice_to_le_u32(buf: &[u8]) -> u32 {

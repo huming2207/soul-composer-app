902cd35b524f2a74c11cc1330b5d58d1fde39a1c
Jackson Ming Hu <huming2207@gmail.com>
2023-08-07 22:36:11 +1000

    Ditch tauri

--- src-tauri/src/device/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod error;
-pub mod packet;
-pub mod proto_codec;
-pub mod serial_comm;
-pub mod serial_detect;
--- src-tauri/src/prog/arm/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod algorithm_binary;
-pub mod arm_error;
-pub mod flash_device;
-pub mod flash_stub_gen;
-pub mod memory_range;

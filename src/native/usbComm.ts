d0e7786c0a5048e9d24b22862ef947f9035d1930
Jackson Ming Hu <huming2207@gmail.com>
2023-08-19 22:34:28 +1000

    Code cleamup

--- src/native/usbComm.ts
+++ src/native/usbComm.ts
@@ -37,25 +37,3 @@ export interface FirmwareMetadata {
   len: number;
   name: string;
 }
-
-export interface FlashAlgoMetadata {
-  name: string;
-  description: string;
-  default: boolean;
-  instructions: string;
-  pcInit?: number;
-  pcUninit?: number;
-  pcProgramPage: number;
-  pcEraseSector: number;
-  pcEraseAll?: number;
-  dataSectionOffset: number;
-  flashStartAddr: number;
-  flashEndAddr: number;
-  flashPageSize: number;
-  erasedByteValue: number;
-  flashSectorSize: number;
-  programTimeout: number;
-  eraseTimeout: number;
-  ramSize: number;
-  flashSize: number;
-}
